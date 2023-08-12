ac01d1da747bc787e8c81bbaa63daa093f9eb7a8
AbhishekRauthan909 <77090876+AbhishekRauthan909@users.noreply.github.com>
2023-08-12 23:36:33 +0530

    fifth program

---
+++ p5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<fcntl.h>
+int main()
+{
+   creat("f1.txt",0666);
+   creat("f2.txt",0666);
+   creat("f3.txt",0666);
+   creat("f4.txt",0666);
+   creat("f5.txt",0666);
+   while(1)
+   { 
+   
+   }
+   return 0;
+}
